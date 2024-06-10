5fdc0686a41ee742952ad71c1e719a8049ad9246
Ihor Didyk <ihordidyk95@gmail.com>
2024-06-10 09:06:48 +0300

    initial commit: create base porject

---
+++ src/main/java/com/github/ih0rd/app/ReactivePlaceholderCliApplication.java
@@ -0,0 +1,13 @@
+package com.github.ih0rd.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivePlaceholderCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactivePlaceholderCliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ih0rd/app/ReactivePlaceholderCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ih0rd.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivePlaceholderCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

863c1d825e1ea3791aef263321278021587c5621
Ihor Didyk <ihordidyk95@gmail.com>
2024-06-12 09:16:16 +0300

    clean up user response for output

--- src/main/java/com/github/ih0rd/app/models/User.java
+++ src/main/java/com/github/ih0rd/app/models/User.java
@@ -4,17 +4,5 @@ import lombok.Builder;
 
 @Builder
 public record User(
-        int id, String name, String username, String email, Address address, String phone, String website,
-        Company company) {
-    @Builder
-    public record Address(String street, String suite, String city, String zipcode, Geo geo) {
-    }
-
-    @Builder
-    public record Geo(String lat, String lng) {
-    }
-
-    @Builder
-    public record Company(String name, String catchPhrase, String bs) {
-    }
+        int id, String name, String username, String email, String phone, String website) {
 }
